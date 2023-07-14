1359adfee57c07b259d03531584f6173ce8c9d30
Skyler Giannetti <sjg5901@psu.edu>
2023-07-14 20:59:52 +0000

    Max minus min challenge complete

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -1,4 +1,5 @@
 
+import java.util.Collections;
 import java.util.List;
 
 public class MaxMinusMin {
@@ -10,6 +11,6 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        return Collections.max(nums) - Collections.min(nums);
     }
 }
